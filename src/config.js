6857f83a36b1652226cac299e39039a479364eaa
jebeni777 <46410301+jebeni777@users.noreply.github.com>
2019-10-15 11:46:17 -0700

    Setting up app, connected to backend

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+        REGION: "us-east-2",
+        BUCKET: "notes-app-uploads-jebeni777"
+    },
+    apiGateway: {
+        REGION: "us-east-2",
+        URL: "https://n15533w58c.execute-api.us-east-2.amazonaws.com/prod"
+    },
+    cognito: {
+        REGION: "us-east-2",
+        USER_POOL_ID: "us-east-2_zBhwErYrH",
+        APP_CLIENT_ID: "24pcb92uc4ukle2avo6vnoje5l",
+        IDENTITY_POOL_ID: "us-east-2:1d75f482-be44-473d-bce2-ca0ca0707620"
+    }
+};
